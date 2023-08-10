1050e28982d1657f6df53f6485850770cae1cdf5
Nestor <nestorpuentesin@gmail.com>
2023-08-10 13:55:58 -0600

    agregue ruta a api a pagina welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
 </head>
+
 <body>
-    <h1> PRUEBA 3 </h1>
-   
-    <div style="border: 5px solid black" >
-        <img src="https://www.ayuntamientogp.imagendigitalstudio.com/img/logo.png" style="display: block; width: 20%; margin: auto;" height="100" width="250" alt="">
-    </div>
+   <h1> PRUEBA 3 </h1>
+   <a href="https://api.gomezpalacio.gob.mx/api/becas/schools" target="_blank"
+      rel="noopener noreferrer">API-becas/escuelas</a>
+
+   <div style="border: 5px solid black">
+      <img src="https://www.ayuntamientogp.imagendigitalstudio.com/img/logo.png"
+         style="display: block; width: 20%; margin: auto;" height="100" width="250" alt="">
+   </div>
 </body>
+
 </html>
