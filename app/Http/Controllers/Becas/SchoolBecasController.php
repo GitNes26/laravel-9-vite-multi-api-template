33ae47745445adb32d02cd25ae5de4616b82945a
Nestor <nestorpuentesin@gmail.com>
2023-08-31 16:12:25 -0600

    separe  api-becas (rutas) a un archivo

--- app/Http/Controllers/Becas/SchoolBecasController.php
+++ app/Http/Controllers/Becas/SchoolBecasController.php
@@ -22,15 +22,14 @@ class SchoolBecasController extends Controller
         $response->data = ObjResponse::DefaultResponse();
         try {
             $list = School::where('schools.active', true)
-            ->join('cities', 'schools.city_id', '=', 'cities.id')
-            ->join('colonies', 'schools.colony_id', '=', 'colonies.id')
-            ->select('schools.*', 'cities.city', 'colonies.colony')
-            ->orderBy('schools.code', 'asc')->get();
+                ->join('cities', 'schools.city_id', '=', 'cities.id')
+                ->join('colonies', 'schools.colony_id', '=', 'colonies.id')
+                ->select('schools.*', 'cities.city', 'colonies.colony')
+                ->orderBy('schools.code', 'asc')->get();
             $response->data = ObjResponse::CorrectResponse();
             $response->data["message"] = 'Peticion satisfactoria | Lista de escuelas.';
             $response->data["result"] = $list;
-        }
-        catch (\Exception $ex) {
+        } catch (\Exception $ex) {
             $response->data = ObjResponse::CatchResponse($ex->getMessage());
         }
         return response()->json($response, $response->data["status_code"]);
@@ -46,13 +45,12 @@ class SchoolBecasController extends Controller
         $response->data = ObjResponse::DefaultResponse();
         try {
             $list = School::where('active', true)
-            ->select('schools.id as value', 'schools.school as text')
-            ->orderBy('schools.school', 'asc')->get();
+                ->select('schools.id as value', 'schools.school as text')
+                ->orderBy('schools.school', 'asc')->get();
             $response->data = ObjResponse::CorrectResponse();
             $response->data["message"] = 'Peticion satisfactoria | Lista de escuelas';
             $response->data["result"] = $list;
-        }
-        catch (\Exception $ex) {
+        } catch (\Exception $ex) {
             $response->data = ObjResponse::CatchResponse($ex->getMessage());
         }
         return response()->json($response, $response->data["status_code"]);
@@ -83,8 +81,7 @@ class SchoolBecasController extends Controller
             $response->data = ObjResponse::CorrectResponse();
             $response->data["message"] = 'peticion satisfactoria | escuela registrado.';
             $response->data["alert_text"] = 'Escuela registrada';
-        }
-        catch (\Exception $ex) {
+        } catch (\Exception $ex) {
             $response->data = ObjResponse::CatchResponse($ex->getMessage());
         }
         return response()->json($response, $response->data["status_code"]);
@@ -99,18 +96,17 @@ class SchoolBecasController extends Controller
     public function show(int $id, Response $response)
     {
         $response->data = ObjResponse::DefaultResponse();
-        try{
+        try {
             $colony = School::where('schools.id', $id)
-            ->join('cities', 'schools.city_id', '=', 'cities.id')
-            ->join('colonies', 'schools.colony_id', '=', 'colonies.id')
-            ->select('schools.*', 'cities.city', 'colonies.colony')
-            ->first();
-            
+                ->join('cities', 'schools.city_id', '=', 'cities.id')
+                ->join('colonies', 'schools.colony_id', '=', 'colonies.id')
+                ->select('schools.*', 'cities.city', 'colonies.colony')
+                ->first();
+
             $response->data = ObjResponse::CorrectResponse();
             $response->data["message"] = 'peticion satisfactoria | escuela encontrado.';
             $response->data["result"] = $colony;
-        }
-        catch (\Exception $ex) {
+        } catch (\Exception $ex) {
             $response->data = ObjResponse::CatchResponse($ex->getMessage());
         }
         return response()->json($response, $response->data["status_code"]);
@@ -127,23 +123,22 @@ class SchoolBecasController extends Controller
         $response->data = ObjResponse::DefaultResponse();
         try {
             $colony = School::where('id', $request->id)
-            ->update([
-                'code' => $request->code,
-                'school' => $request->school,
-                'city_id' => $request->city_id,
-                'colony_id' => $request->colony_id,
-                'address' => $request->address,
-                'tel' => $request->tel ?? 'S/N',
-                'director' => $request->director,
-                'loc_for' => $request->loc_for,
-                'zone' => $request->zone,
-                // // 'type' => $request->type,
-            ]);
+                ->update([
+                    'code' => $request->code,
+                    'school' => $request->school,
+                    'city_id' => $request->city_id,
+                    'colony_id' => $request->colony_id,
+                    'address' => $request->address,
+                    'tel' => $request->tel ?? 'S/N',
+                    'director' => $request->director,
+                    'loc_for' => $request->loc_for,
+                    'zone' => $request->zone,
+                    // // 'type' => $request->type,
+                ]);
 
             $response->data = ObjResponse::CorrectResponse();
             $response->data["message"] = 'peticion satisfactoria | escuela actualizada.';
             $response->data["alert_text"] = 'Escuela actualizada';
-
         } catch (\Exception $ex) {
             $response->data = ObjResponse::CatchResponse($ex->getMessage());
         }
@@ -161,14 +156,13 @@ class SchoolBecasController extends Controller
         $response->data = ObjResponse::DefaultResponse();
         try {
             School::where('id', $id)
-            ->update([
-                'active' => false,
-                'deleted_at' => date('Y-m-d H:i:s'),
-            ]);
+                ->update([
+                    'active' => false,
+                    'deleted_at' => date('Y-m-d H:i:s'),
+                ]);
             $response->data = ObjResponse::CorrectResponse();
             $response->data["message"] = 'peticion satisfactoria | escuela eliminada.';
-            $response->data["alert_text"] ='Escuela eliminada';
-
+            $response->data["alert_text"] = 'Escuela eliminada';
         } catch (\Exception $ex) {
             $response->data = ObjResponse::CatchResponse($ex->getMessage());
         }
@@ -181,45 +175,46 @@ class SchoolBecasController extends Controller
      * @param  
      * @return \Illuminate\Http\Response $response
      */
-    public function checkAvailableData(string $table, string $column, any $value, string $propTitle, string $input, int $id, any $secondTable=null) {
+    public function checkAvailableData(string $table, string $column, string $value, string $propTitle, string $input, int $id, string $secondTable = null)
+    {
 
-		// if ($secondTable) {
-		// 	$query = "SELECT count(*) as duplicate FROM $table INNER JOIN $secondTable ON user_id=users.id WHERE $column='$value' AND active=1;";
-		// 	if ($id != null) $query = "SELECT count(*) as duplicate FROM $table t INNER JOIN $secondTable ON t.user_id=users.id WHERE t.$column='$value' AND active=1 AND t.id!=$id";
-		// } else {
-		// 	$query = "SELECT count(*) as duplicate FROM $table WHERE $column='$value' AND active=1";
+        // if ($secondTable) {
+        // 	$query = "SELECT count(*) as duplicate FROM $table INNER JOIN $secondTable ON user_id=users.id WHERE $column='$value' AND active=1;";
+        // 	if ($id != null) $query = "SELECT count(*) as duplicate FROM $table t INNER JOIN $secondTable ON t.user_id=users.id WHERE t.$column='$value' AND active=1 AND t.id!=$id";
+        // } else {
+        // 	$query = "SELECT count(*) as duplicate FROM $table WHERE $column='$value' AND active=1";
 
-			
-		// 	if ($id != null) $query = "SELECT count(*) as duplicate FROM $table WHERE $column='$value' AND active=1 AND id!=$id";
-		// }
 
-		// $consulta = $this->Select($query,false);
-		// if ($consulta["duplicate"] > 0) {
-		// 	$response = array(
-		// 		"result" => true,
-		// 		"alert_icon" => 'warning',
-		// 		"alert_title" => "$propTitle no esta disponible!",
-		// 		"alert_text" => "<b>$value</b> ya existe, intenta con uno diferente.",
-		// 		"message" => "duplicado",
-		// 		"input" => $input
-		// 	);
-		// 	// $response = $this->duplicateResponse($propTitle, $value, $input);
-		// } else {
-		// 	$response = array(
-		// 		"result" => false,
-		// 	);
-		// }
-		// return $response;
-        
-        
-        
-        
+        // 	if ($id != null) $query = "SELECT count(*) as duplicate FROM $table WHERE $column='$value' AND active=1 AND id!=$id";
+        // }
+
+        // $consulta = $this->Select($query,false);
+        // if ($consulta["duplicate"] > 0) {
+        // 	$response = array(
+        // 		"result" => true,
+        // 		"alert_icon" => 'warning',
+        // 		"alert_title" => "$propTitle no esta disponible!",
+        // 		"alert_text" => "<b>$value</b> ya existe, intenta con uno diferente.",
+        // 		"message" => "duplicado",
+        // 		"input" => $input
+        // 	);
+        // 	// $response = $this->duplicateResponse($propTitle, $value, $input);
+        // } else {
+        // 	$response = array(
+        // 		"result" => false,
+        // 	);
+        // }
+        // return $response;
+
+
+
+
         // function validateAvailableData($cellphone, $id) {
         //    // #VALIDACION DE DATOS REPETIDOS
         //    $duplicate = $this->checkAvailableData('candidates', 'cellphone', $cellphone, 'El número celular', 'input_cellphone', $id, 'users');
         //    if ($duplicate["result"] == true) die(json_encode($duplicate));
         // }
-     
+
         // function getIdByUserId($user_id, $private=true) {
         //    $query = "SELECT id FROM candidates WHERE user_id=$user_id;";
         //    $candidate = $this->Select($query, false);
@@ -233,6 +228,5 @@ class SchoolBecasController extends Controller
         //       else die(json_encode(array("data" => $candidate["id"]))); 
         //    }
         // }
- 	}
-
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,15 +6,7 @@ use App\Http\Controllers\UsuarioController;
 use App\Http\Controllers\RequisicionController;
 use App\Http\Controllers\oficiosController;
 
-#region CONTROLLERS BECAS
-use App\Http\Controllers\becas\RoleBecasController;
-use App\Http\Controllers\becas\UserBecasController;
-use App\Http\Controllers\becas\PerimeterBecasController;
-use App\Http\Controllers\becas\CityBecasController;
-use App\Http\Controllers\becas\ColonyBecasController;
-use App\Http\Controllers\becas\LevelBecasController;
-use App\Http\Controllers\becas\SchoolBecasController;
-#endregion CONTROLLERS BECAS
+
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -23,84 +15,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 // Route::api('usuario',[UsuarioController::class,'index']);
 
-Route::apiResource('usuario',UsuarioController::class);
-Route::apiResource('requis',RequisicionController::class);
-Route::post('pdf', [ oficiosController::class,'pdf']);
-Route::get('pdf', function(){
-    return 1 ;
+Route::apiResource('usuario', UsuarioController::class);
+Route::apiResource('requis', RequisicionController::class);
+Route::post('pdf', [oficiosController::class, 'pdf']);
+Route::get('pdf', function () {
+    return 1;
 });
 
 Route::prefix('becas')->group(function () {
-    Route::get('/', function(){return 'becas' ;});
-    Route::post('/login', [UserBecasController::class,'login']);
-    Route::post('/signup', [UserBecasController::class,'signup']);
-    
-    // Route::middleware('auth:sanctum')->group(function () {
-        // Route::get('/getUser/{token}', [UserBecasController::class,'getUser']); //cerrar sesión (eliminar los tokens creados)
-        Route::delete('/logout/{id}', [UserBecasController::class,'logout']); //cerrar sesión (eliminar los tokens creados)
-
-        Route::controller(UserBecasController::class)->group(function () {
-            Route::get('/users','index');
-            Route::get('/users/selectIndex','selectIndex');
-            Route::get('/users/{id}','show');
-            Route::post('/users','create');
-            Route::put('/users','update');
-            Route::delete('/users/{id}','destroy');
+    Route::get('/', function () {
+        return 'becas';
         });
-
-        Route::controller(RoleBecasController::class)->group(function () {
-            Route::get('/roles','index');
-            Route::get('/roles/selectIndex','selectIndex');
-            Route::get('/roles/{id}','show');
-            Route::post('/roles','create');
-            Route::put('/roles','update');
-            Route::delete('/roles/{id}','destroy');
-        });
-
-        Route::controller(PerimeterBecasController::class)->group(function () {
-            Route::get('/perimeters','index');
-            Route::get('/perimeters/selectIndex','selectIndex');
-            Route::get('/perimeters/{id}','show');
-            Route::post('/perimeters','create');
-            Route::put('/perimeters','update');
-            Route::delete('/perimeters/{id}','destroy');
-        });
-
-        Route::controller(CityBecasController::class)->group(function () {
-            Route::get('/cities','index');
-            Route::get('/cities/selectIndex','selectIndex');
-            Route::get('/cities/{id}','show');
-            Route::post('/cities','create');
-            Route::put('/cities','update');
-            Route::delete('/cities/{id}','destroy');
-        });
-        
-        Route::controller(ColonyBecasController::class)->group(function () {
-            Route::get('/colonies','index');
-            Route::get('/colonies/selectIndex','selectIndex');
-            Route::get('/colonies/{id}','show');
-            Route::post('/colonies','create');
-            Route::put('/colonies','update');
-            Route::delete('/colonies/{id}','destroy');
-        });
-
-        Route::controller(LevelBecasController::class)->group(function () {
-            Route::get('/levels','index');
-            Route::get('/levels/selectIndex','selectIndex');
-            Route::get('/levels/{id}','show');
-            Route::post('/levels','create');
-            Route::put('/levels','update');
-            Route::delete('/levels/{id}','destroy');
-        });
-        
-        Route::controller(SchoolBecasController::class)->group(function () {
-            Route::get('/schools','index');
-            Route::get('/schools/{id}','show');
-            Route::post('/schools','create');
-            Route::put('/schools','update');
-            Route::delete('/schools/{id}','destroy');
-        });
-        
-        
-    // });
+    include_once "becas.routes.php";
 });
