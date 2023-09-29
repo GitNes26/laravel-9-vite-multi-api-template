b35048c5da7ffef1823f0fc6020c648203fb5b61
Samuel Garza <samuel.garza29@hotmail.com>
2023-09-29 10:12:38 -0600

    se creo la ruta facturacion con su controlador y su api.routes

---
+++ app/Http/Controllers/Facturaciones/FactuacionController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Facturaciones;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FactuacionController extends Controller
+{
+    
+}
---
+++ routes/facturacion.routes.php
@@ -0,0 +1,6 @@
+<?php
+
+use App\Http\Controllers\Facturaciones\FactuacionController;
+use Illuminate\Routing\Route;
+
+Route::get('/facturas', [FactuacionController::class, 'index']);
