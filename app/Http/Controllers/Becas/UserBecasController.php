11e1278725981ed8918d3c8209626ddb3b0059c0
Nestor <nestorpuentesin@gmail.com>
2023-10-06 16:15:51 -0600

    agregue validacion unica en codigo

--- app/Http/Controllers/Becas/UserBecasController.php
+++ app/Http/Controllers/Becas/UserBecasController.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
+use Symfony\Component\VarDumper\VarDumper;
 
 class UserBecasController extends Controller
 {
@@ -164,15 +165,18 @@ class UserBecasController extends Controller
     {
         $response->data = ObjResponse::DefaultResponse();
         try {
-            $token = $request->bearerToken();
+            // $token = $request->bearerToken();
+
+            $duplicate = $this->validateAvailableData($request->username, $request->email, null);
+            if ($duplicate["result"] == true) {
+                $response->data = $duplicate;
+                return response()->json($response);
+            }
 
             $new_user = User::create([
-                // 'name' => $request->name,
-                // 'last_name' => $request->last_name,
                 'email' => $request->email,
                 'username' => $request->username,
                 'password' => Hash::make($request->password),
-                // 'phone' => $request->phone,
                 'role_id' => $request->role_id,
             ]);
             $response->data = ObjResponse::CorrectResponse();
@@ -219,6 +223,12 @@ class UserBecasController extends Controller
     {
         $response->data = ObjResponse::DefaultResponse();
         try {
+            $duplicate = $this->validateAvailableData($request->username, $request->email, $request->id);
+            if ($duplicate["result"] == true) {
+                $response->data = $duplicate;
+                return response()->json($response);
+            }
+
             $user = User::where('id', $request->id)
                 ->update([
                     // 'name' => $request->name,
@@ -265,17 +275,45 @@ class UserBecasController extends Controller
 
 
 
-    // private function validateAvailability(string $prop, int $value, string $message_error)
-    // {
-    //     $response->data = ObjResponse::DefaultResponse();
-    //     data_set($response, 'alert_text', $message_error);
-    //     try {
-    //         $exist = User::where($prop, $value)->count();
 
-    //         if ($exist > 0) $response = ObjResponse::CorrectResponse();
-    //     } catch (\Exception $ex) {
-    //         $response = ObjResponse::CatchResponse($ex->getMessage());
-    //     }
-    //     return response()->json($response, $response["status_code"]);
-    // }
+    private function validateAvailableData($username, $email, $id)
+    {
+        // #VALIDACION DE DATOS REPETIDOS
+        $duplicate = $this->checkAvailableData('users', 'username', $username, 'El nombre de usuario', 'username', $id, null);
+        if ($duplicate["result"] == true) return $duplicate;
+        $duplicate = $this->checkAvailableData('users', 'email', $email, 'El correo electrónico', 'email', $id, null);
+        if ($duplicate["result"] == true) return $duplicate;
+        return array("result" => false);
+    }
+
+    public function checkAvailableData($table, $column, $value, $propTitle, $input, $id, $secondTable = null)
+    {
+        if ($secondTable) {
+            $query = "SELECT count(*) as duplicate FROM $table INNER JOIN $secondTable ON user_id=users.id WHERE $column='$value' AND active=1;";
+            if ($id != null) $query = "SELECT count(*) as duplicate FROM $table t INNER JOIN $secondTable ON t.user_id=users.id WHERE t.$column='$value' AND active=1 AND t.id!=$id";
+        } else {
+            $query = "SELECT count(*) as duplicate FROM $table WHERE $column='$value' AND active=1";
+            if ($id != null) $query = "SELECT count(*) as duplicate FROM $table WHERE $column='$value' AND active=1 AND id!=$id";
+        }
+        $result = DB::connection('mysql_becas')->select($query)[0];
+        // var_dump($result->duplicate);
+        if ((int)$result->duplicate > 0) {
+            // echo "entro al duplicate";
+            $response = array(
+                "result" => true,
+                "status_code" => 409,
+                "alert_icon" => 'warning',
+                "alert_title" => "$propTitle no esta disponible!",
+                "alert_text" => "$propTitle no esta disponible! - $value ya existe, intenta con uno diferente.",
+                "message" => "duplicate",
+                "input" => $input,
+                "toast" => false
+            );
+        } else {
+            $response = array(
+                "result" => false,
+            );
+        }
+        return $response;
+    }
 }
