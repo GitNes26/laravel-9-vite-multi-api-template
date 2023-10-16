a143f40f685681842d85cb175906c8c30390a78a
PSONTE <101921010+PSONTE@users.noreply.github.com>
2023-10-16 15:55:13 -0600

    en el result de login se envia usuario encontrado en el result

--- app/Http/Controllers/GomezApp/UserController.php
+++ app/Http/Controllers/GomezApp/UserController.php
@@ -45,7 +45,7 @@ class UserController extends Controller
          $response->data = ObjResponse::CorrectResponse();
          $response->data["message"] = 'peticion satisfactoria | usuario logeado.';
          //  $response->data["result"]["token"] = $token;
-         $response->data["result"]["user"]["id"] = $user->id;
+         $response->data["result"] = $user;
          return response()->json($response, $response->data["status_code"]);
       }
    }
